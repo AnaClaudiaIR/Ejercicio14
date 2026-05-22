e939b3bd427bdf92ed3b19d31eecdf58175e94ff
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-05-22 18:05:22 +0200

    subir1

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -1,42 +1,38 @@
-package org.example;
+/*package org.example;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-//import java.awt.*;
-//import java.awt.TextField; --> No se necesitan estos imports
-import javafx.scene.control.*;
-
-import java.util.Optional;
 
 
-public class Main extends Application {
+public class Main2 extends Application {
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Número 10 dígitos");
+        primaryStage.setTitle("DNI");
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
 
-        Label numeroLabel = new Label("Número: ");
-        TextField numeroField = new TextField();
-        gridPane.add(numeroLabel, 0, 0);
-        gridPane.add(numeroField, 1, 0);
+        Label dniLabel = new Label("DNI: ");
+        TextField dniField = new TextField();
+        gridPane.add(dniLabel, 0, 0);
+        gridPane.add(dniField, 1, 0);
 
         Button aceptarButton = new Button("Confirmar");
         gridPane.add(aceptarButton, 1, 1);
 
         aceptarButton.setOnAction(e -> {
-            String numero = numeroField.getText();
-            if (numero.length() != 10) {
+            String numero = dniLabel.getText();
+            if (!numero.matches("\\d{8}[A-Z]")) {
                 Alert alert = new Alert(Alert.AlertType.WARNING);
                 alert.setTitle("Error");
                 alert.setHeaderText(null);
-                alert.setContentText("El campo debe tener 10 caracteres.");
+                alert.setContentText("Los datos no han sido introducidos correctamente.");
                 alert.showAndWait();
             } else {
                 System.out.println("Número: " + numero);
@@ -51,3 +47,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+*/
